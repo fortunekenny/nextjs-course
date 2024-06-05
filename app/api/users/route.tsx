f2bdb9bfcd54a60dcc54d31344129b31affc96fa
fortunekenny <gbephraim@gmail.com>
2024-06-05 10:16:36 +0100

    api

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,6 +1,21 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+
 
 
 export function GET(request: NextRequest){
-    return NextResponse.json('hello');
+    return NextResponse.json([
+        {id: 1, name: 'Ken'},
+        {id: 2, name: 'Tab'}
+    ]);
+}
+
+
+export async function POST(request: NextResponse){
+    const body = await request.json()
+    const validation = schema.safeParse(body)
+        if (!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400})
+    return NextResponse.json({id: 1, name: body.name}, {status: 201})
 }
