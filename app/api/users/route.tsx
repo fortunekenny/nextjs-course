9f3686029f4f968a32adc22cd6eb72335067ba00
fortunekenny <gbephraim@gmail.com>
2024-06-09 21:49:36 +0100

    db integratn with prisma

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,14 +1,14 @@
-import { error } from "console";
+
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
+import { error } from "console";
 
 
 
-export function GET(request: NextRequest){
-    return NextResponse.json([
-        {id: 1, name: 'Ken'},
-        {id: 2, name: 'Tab'}
-    ]);
+export async function GET(request: NextRequest){
+    const users = await prisma.user.findMany()
+    return NextResponse.json(users);
 }
 
 
@@ -17,5 +17,21 @@ export async function POST(request: NextResponse){
     const validation = schema.safeParse(body)
         if (!validation.success)
         return NextResponse.json(validation.error.errors, {status: 400})
-    return NextResponse.json({id: 1, name: body.name}, {status: 201})
+
+        const user = await prisma.user.findUnique({
+            where: {email: body.email}
+        })
+
+        if(user){
+            return NextResponse.json({error: 'User already exist'},{status: 400})
+        }
+
+        const newUser = await prisma.user.create({
+            data: {
+                name: body.name,
+                email: body.email
+            }
+        })
+
+    return NextResponse.json(newUser, {status: 201})
 }
